d11adf733240a34e0c8ad575233d969dbef4ac22
Viktor <enotcode@gmail.com>
2017-04-06 03:26:34 +0300

    update👻

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
+import os
+import config
+
 from flask import render_template
 
 from . import app
 
+hi = os.environ.get("hi", 5000)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', hello="Flask Starter")
-
-
+    return render_template('index.html', hello=hi)
